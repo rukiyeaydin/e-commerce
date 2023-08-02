fa73c03dc01083e829a0a60fc81765c5d1bbd76d
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-02 09:35:27 +0300

    new route added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Anasayfa from './routes/Anasayfa';
+import Kadin from './routes/Kadin';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<Anasayfa/>} />
+      <Route path="/Kadin" element={<Kadin/>} />
     </Routes>
     </>
   );
