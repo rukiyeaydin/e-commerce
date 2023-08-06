746b52660c4d7b81d006bf7c2703ee107d1859de
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-06 11:48:21 +0300

    login section

---
+++ src/routes/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Loginhero from "../components/Loginhero";
+
+
+const Login = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Loginhero/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Login
