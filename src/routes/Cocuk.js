4489cb87e58ed1061ce51a369eb8341f1ad51b6f
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-06 11:47:32 +0300

    cocuk section

---
+++ src/routes/Cocuk.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Herococuk from '../components/Herococuk';
+import Footer from "../components/Footer";
+
+const Cocuk = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Herococuk/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Cocuk
