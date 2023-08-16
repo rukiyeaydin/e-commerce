fa25c7dd33e53bb00d4c055e679cd703dda9f02d
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-08-16 20:00:48 +0300

    Create data.js

---
+++ src/components/Data.js
@@ -0,0 +1,83 @@
+import etnik from '../images/kadin/ethnic.webp';
+import greendress from '../images/kadin/greendress.webp';
+import micky from '../images/kadin/micky.jpeg';
+import tunic from '../images/kadin/tunic.webp';
+import white from '../images/kadin/white.webp';
+
+
+const data = {
+    productItems:[
+        {
+            id: "1",
+            ad: "Etnik Desenli Elbise",
+            exprice: "1000",
+            price: "600",
+            image: etnik,
+        },
+        {
+            id: "2",
+            ad: "Yeşil Uzun Elbise",
+            exprice: "1500",
+            price: "899.9",
+            image: greendress,
+        },
+        {
+            id: "3",
+            ad: "Micky Mouse Tişört",
+            exprice: "1120",
+            price: "650",
+            image: micky,
+        },
+        {
+            id: "4",
+            ad: "Arkası Desenli Elbise",
+            exprice: "1200",
+            price: "699.9",
+            image: tunic,
+        },
+        {
+            id: "5",
+            ad: "Beyaz Uzun Elbise",
+            exprice: "800",
+            price: "710",
+            image: white,
+        },
+        {
+            id: "6",
+            ad: "Etnik Desenli Elbise",
+            exprice: "1000",
+            price: "600",
+            image: etnik,
+        },
+        {
+            id: "7",
+            ad: "Yeşil Uzun Elbise",
+            exprice: "1500",
+            price: "899.9",
+            image: greendress,
+        },
+        {
+            id: "8",
+            ad: "Micky Mouse Tişört",
+            exprice: "1120",
+            price: "650",
+            image: micky,
+        },
+        {
+            id: "9",
+            ad: "Arkası Desenli Elbise",
+            exprice: "1200",
+            price: "699.9",
+            image: tunic,
+        },
+        {
+            id: "10",
+            ad: "Beyaz Uzun Elbise",
+            exprice: "800",
+            price: "710",
+            image: white,
+        },
+    ]
+}
+
+export default data; //export etmeyi unutma!!
